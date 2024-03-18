3b2cd092725f05b7665d6e5cda1da8cb8e4b07e3
Simone <156814996+Simone-Pangardi@users.noreply.github.com>
2024-03-18 16:17:49 -0300

    Update scripts.js
    Correção de texto

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 let typed = new Typed('.js-words', {
-    strings:['Study', 'Practical', 'Front End'],
+    strings:['studying', 'practicing', 'Front End'],
     typeSpeed: 150,
     backSpeed: 150,
     loop: true
